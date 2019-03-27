3ec32f43727ae04b5bac73f274caf28e64c61e17
pritam-patil <pritam18121990@gmail.com>
2019-03-27 22:19:39 +0530

    Update UI to match SU.com design
    Changes:
    - Added navigation matching SU toolbar
    - Update Appbar matching SU appbar

--- src/components/core/navigator.jsx
+++ src/components/core/navigator.jsx
@@ -4,35 +4,35 @@ import './styles.css';
 
 const Navigator = props => (
   <div className="home-page">
-    <div class='snackbarWrapper'>
-        <div className="snackbar">
-          <div className="sb-left">
-            <i class="fab fa-connectdevelop fa-2x"></i>
-            {props.picker}
-          </div>
-          <div className="sb-right">
-            <i class="fas fa-thumbs-up fa-1g border"> {props.likes} </i>
-            <i class="fas fa-ellipsis-v fa-1g center"></i>
-          </div>
+    <div className="snackbarWrapper">
+      <div className="snackbar">
+        <div className="sb-left">
+          <i class="fab fa-connectdevelop fa-2x"></i>
+          {props.picker}
         </div>
+        <div className="sb-right">
+          <i class="fas fa-thumbs-up fa-1g border"> {props.likes} </i>
+          <i class="fas fa-ellipsis-v fa-1g center"></i>
+        </div>
+      </div>
     </div>    
     <div className="main-content">{props.children}</div>
     <div className='footerWrapper'> 
-        <footer>
-          <i class="fas fa-reply fa-2x" onClick={props.onPrevious}></i>
-          <i class="far fa-thumbs-down fa-2x" onClick={props.onDislike}></i>
-          <div id="reddit" onClick={props.onClick}>
-            <i class="fab fa-reddit fa-4x"></i>
-          </div>
-          <i class="far fa-thumbs-up fa-2x"></i>
-          <i class="fas fa-share-alt fa-2x" onClick={props.onShareClick}></i>
-        </footer>
+      <footer>
+        <i class="fas fa-reply fa-2x" onClick={props.onPrevious}></i>
+        <i class="far fa-thumbs-down fa-2x" onClick={props.onDislike}></i>
+        <div id="reddit" onClick={props.onClick}>
+          <i class="fab fa-reddit fa-4x"></i>
+        </div>
+        <i class="far fa-thumbs-up fa-2x"></i>
+        <i class="fas fa-share-alt fa-2x" onClick={props.onShareClick}></i>
+      </footer>
     </div>
   </div>
 );
 
-Navigator.propTypes = ({
+Navigator.propTypes = {
   onShareClick: PropTypes.func.isRequired
-});
+};
 
 export default Navigator;
